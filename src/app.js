04f416711485edca8f1b10cd5bff1eef625dae0d
Stephan Kim <hhkimstar1226@gmail.com>
2025-11-17 14:09:04 +0900

    chore: 개발용 CORS 설정

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
